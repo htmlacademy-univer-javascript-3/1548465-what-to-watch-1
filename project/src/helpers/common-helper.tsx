7c00d66f466533abda8b16b08dd1a577e4a7a0e5
AydartsyanGrisha <aydartsyang@gmail.com>
2023-01-19 21:08:42 +0500

    Fixed review date

--- project/src/helpers/common-helper.tsx
+++ project/src/helpers/common-helper.tsx
@@ -3,3 +3,13 @@ export function getRunTime(runTime: number): string {
   const hours = Math.floor(runTime / 60);
   return `${hours}h ${minutes}m`;
 }
+
+export function getFormattedReviewDate(date: Date): string {
+  const months = ['January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
+  const day = date.getDay();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+  return `${month} ${day}, ${year}`;
+}
